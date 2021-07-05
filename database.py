4f559edec6d4574eb92e79c1e281837f92be9824
q <aorew@yandex.ru>
2021-07-05 20:15:58 +0300

    adding files

---
+++ database.py
@@ -0,0 +1,14 @@
+import os
+
+import psycopg2
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+CONNECTION = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+def db_query(line, fetching = True):
+    with CONNECTION:
+        with CONNECTION.cursor() as cur:
+            cur.execute(line)
+            if fetching:
+                data = cur.fetchall()
+                return data
