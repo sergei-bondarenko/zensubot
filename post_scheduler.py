40843d68c99884fc3389c22b5da2bf413f1b46e0
Sergei Bondarenko <sergei@bondarenko.xyz>
2021-07-05 20:33:20 +0100

    Schedule a post every 5 days.

--- post_scheduler.py
+++ post_scheduler.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 
 def callback_minute(context):
-    context.bot.send_message(chat_id='@guhccssa', text='Тестирую отправку сообщения каждые 10 секунд.')
+    # context.bot.send_message(chat_id='@guhccssa', text='Тестирую отправку сообщения каждые 10 секунд.')
+    posted_message = context.bot.copy_message(chat_id='-1001211284566', from_chat_id = '-1001575457049', message_id = 3)
 
 def create_post(job):
     # interval = timedelta(days = 5)
-    interval = timedelta(seconds = 10)
+    interval = timedelta(minutes = 5)
     # job_minute = job.run_repeating(callback = callback_minute, interval = interval, first = datetime(2021, 7, 5, 18, 50))
-    # job_minute = job.run_repeating(callback = callback_minute, interval = interval)
+    job_minute = job.run_repeating(callback = callback_minute, interval = interval)
+
