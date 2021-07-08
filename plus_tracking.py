4f9a7f027a0ff67008be5931bfed34da0be99e7f
aorews <aorew@yandex.ru>
2021-07-08 20:44:30 +0300

    plus

--- plus_tracking.py
+++ plus_tracking.py
@@ -10,11 +10,22 @@ def plus(update, context):
     replied_message = update.message.message_id
     user_id = update.effective_user.id
 
+    to_chat_ids = db_query(
+            f"select parent from chats_connection where child = {chat_id}"
+        )
+    
+    print(to_chat_ids)
+
+    if len(to_chat_ids) == 0:
+        return None
+
     cur_amount, has_voted = db_query(
         f"""select count(1) + 1, coalesce(sum(case when user_id = {user_id} then 1 else 0 end), 0) 
             from plus_data 
             where chat_id = {chat_id} and message_id = {replied_message}"""
     )[0]
+
+    print(cur_amount, has_voted)
 
     if has_voted != 0:
         text = "Ты уже голосовал, грязный и мерзкий!"
@@ -23,17 +34,12 @@ def plus(update, context):
         return None
 
     if cur_amount == THRESHOLD:
-        to_chat_ids = db_query(
-            f"select parent from chats_connection where child = {chat_id}"
-        )
-
         for chat in to_chat_ids:
             context.bot.copy_message(
                 chat_id=chat[0], from_chat_id=chat_id, message_id=replied_message
             )
 
     elif cur_amount < THRESHOLD:
-        print(chat_id)
         post_to = db_query(f"select title from chats where id = {chat_id}")[0][0]
         text = f"{cur_amount} из {THRESHOLD} + до поста в {post_to}"
         bot_message_to_chat(context, chat_id, text, AUTODESTRUCTION, replied_message)
