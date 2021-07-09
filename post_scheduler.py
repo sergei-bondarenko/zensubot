d5c681a58a2e82f2bb738a6a64312089f72e8633
Sergei Bondarenko <sergei@bondarenko.xyz>
2021-07-09 21:38:57 +0100

    Schedule a post every 5 days.

--- post_scheduler.py
+++ post_scheduler.py
@@ -1,6 +1,6 @@
 import logging
 from database import db_query
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +12,7 @@ def callback_minute(context):
     )
     for job_type, created in data:
         logger.info(
-            f"{job_type}: {datetime.now() - created}"
+            f"{job_type}: {datetime.now(timezone.utc) - created}"
         )
 
 def create_post_sc(job):
