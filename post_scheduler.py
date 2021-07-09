e28fc038c2cbb4b41950c6bf52c280e034c42a5d
Sergei Bondarenko <sergei@bondarenko.xyz>
2021-07-09 20:55:27 +0100

    Schedule a post every 5 days.

--- post_scheduler.py
+++ post_scheduler.py
@@ -1,12 +1,21 @@
+import logging
 from datetime import datetime, timedelta
 
 def callback_minute(context):
     # context.bot.send_message(chat_id='@guhccssa', text='Тестирую отправку сообщения каждые 10 секунд.')
-    posted_message = context.bot.copy_message(chat_id='-1001211284566', from_chat_id = '-1001575457049', message_id = 3)
+    job_types = db_query(
+        'select job_type from post_templates',
+        True,
+    )
+    for job_type in job_types:
+        created = db_query(
+            f"select max(created) from jobs where type = {job_type}",
+            True,
+        )
+        logger.info(
+            f"{job_type}: {created}"
+        )
 
 def create_post_sc(job):
-    # interval = timedelta(days = 5)
-    interval = timedelta(minutes = 5)
-    # job_minute = job.run_repeating(callback = callback_minute, interval = interval, first = datetime(2021, 7, 5, 18, 50))
-    # job_minute = job.run_repeating(callback = callback_minute, interval = interval)
-
+    interval = timedelta(minutes = 1)
+    job_minute = job.run_repeating(callback = callback_minute, interval = interval)
