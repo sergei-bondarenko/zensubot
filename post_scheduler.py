4b1f0c306772044ad46886295c8203aef7ef3f73
Sergei Bondarenko <sergei@bondarenko.xyz>
2021-07-09 21:10:01 +0100

    Schedule a post every 5 days.

--- post_scheduler.py
+++ post_scheduler.py
@@ -2,6 +2,8 @@ import logging
 from database import db_query
 from datetime import datetime, timedelta
 
+logger = logging.getLogger(__name__)
+
 def callback_minute(context):
     # context.bot.send_message(chat_id='@guhccssa', text='Тестирую отправку сообщения каждые 10 секунд.')
     data = db_query(
