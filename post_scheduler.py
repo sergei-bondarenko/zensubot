fd9681d31e5017eba30f626ad795a822ffbb3ac8
Sergei Bondarenko <sergei@bondarenko.xyz>
2021-07-09 22:01:55 +0100

    Schedule a post every 5 days.

--- post_scheduler.py
+++ post_scheduler.py
@@ -11,9 +11,20 @@ def callback_minute(context):
         True,
     )
     for job_type, created in data:
-        logger.info(
-            f"{job_type}: {datetime.now(timezone.utc) - created}"
-        )
+        passed_time = datetime.now(timezone.utc) - created
+        if passed_time >= timedelta(days = 5):
+            logger.info(
+                f"{job_type}: {passed_time}"
+            )
+       # photo_id, caption = db_query(
+       #     f'select photo_id, caption from post_templates where job_type = {query.data}',
+       #     True,
+       # )[0]
+       # if photo_id == "None":
+       #     context.bot.send_message(update.effective_chat.id, caption)
+       # else:
+       #     context.bot.send_photo(update.effective_chat.id, photo_id, caption=caption)
+       # context.bot.send_message(update.effective_chat.id, "Отправь новый темплейт.")
 
 def create_post_sc(job):
     interval = timedelta(seconds = 20)
