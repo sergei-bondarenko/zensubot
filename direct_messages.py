a727124ec99a454e74a2f142c23b36a72dc0621a
aorews <aorew@yandex.ru>
2021-07-16 17:40:26 +0300

    fix

--- direct_messages.py
+++ direct_messages.py
@@ -253,10 +253,10 @@ def get_stat(update):
                             order by types_id""")
 
     text = f'<b>Статистика пятидневок <a href="tg://user?id={user_id}">{user_name}</a></b><br><br>'
-    text += f"<pre>Тип     Закончено   Время</pre><br>"
+    text += f"<pre>Тип     Закончено   Время</pre>"
 
     for type, ended, started, sum in query:
-        text += f"<pre>{type}  {ended}/{started}  {int(sum)}</pre><br>"
+        text += f"<pre>{type}  {ended}/{started}  {int(sum)}</pre>"
 
     post_to_telegraph(text)
     return text
