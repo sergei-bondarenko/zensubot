49ae188b27cfb97a11d04e6b5bc51c50940b4b6f
aorews <aorew@yandex.ru>
2021-07-16 13:29:01 +0300

    pic

--- inline.py
+++ inline.py
@@ -16,7 +16,7 @@ def inline_stat(update, context) -> None:
                     input_message_content=InputTextMessageContent(
                         "<b>Медитации</b> 2/3 54h 20m", ParseMode=ParseMode.HTML
                     ),
-                    thumb_url="https://i.imgur.com/o46IfVV.jpeg",
+                    thumb_url="https://i.imgur.com/2G1ZxcU.jpeg",
                     thumb_height=100,
                     thumb_width=100
                 )
