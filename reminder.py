df2bb5943122f1ec137d4f678a43c7320d62ccb6
Sergei Bondarenko <sergei@bondarenko.xyz>
2021-07-18 10:04:25 +0100

    Reminder.

--- reminder.py
+++ reminder.py
@@ -47,7 +47,7 @@ def send_notification(context):
 
 def reminder(job):
     # job.run_daily(callback = send_notification, time = time(POST_HOUR - REMINDER_DELTA, POST_MINUTE), days = REMINDER_DAYS, name = 'reminder_ok')
-    job.run_daily(callback = send_notification, time = time(9, 04), days = [6], name = 'reminder_ok')
+    job.run_daily(callback = send_notification, time = time(9, 5), days = [6], name = 'reminder_ok')
 
     # job.run_repeating(callback = send_notification, interval = timedelta(seconds = 20), name = 'reminder_ok')
     pass
