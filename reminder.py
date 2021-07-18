59880aaab6f88e6f37420eca4944822a69ff030b
Sergei Bondarenko <sergei@bondarenko.xyz>
2021-07-18 17:17:37 +0100

    Reminder.

--- reminder.py
+++ reminder.py
@@ -14,20 +14,45 @@ logger = logging.getLogger(__name__)
 post_time = f"21:00:00"
 
 
-def send_notification(context):
+def send_notification(context, chat_id, message_id, completed_users_query, all_users_query):
+    completed_users = set(db_query(completed_users_query, True))
+    all_users = set(db_query(all_users_query, True))
+    text = ""
+    for user_id in all_users - completed_users:
+        first_name = db_query(
+            f"select first_name from users where id = {user_id[0]}",
+            True,
+        )[0][0]
+        text += f'<a href="tg://user?id={user_id[0]}">{first_name}</a>, '
+    if text != "":
+        text += f"день закончится через {REMINDER_DELTA} часа. Ты не забыл отметиться?"
+        bot_message_to_chat(context, chat_id, text, reply_to_message = message_id, parse_mode = ParseMode.HTML)
+
+
+def reminder_callback(context):
     cur_date = datetime.now()
     yesterday = cur_date - timedelta(days=1)
     yesterday = yesterday.strftime('%Y-%m-%d') + f" {post_time}"
     two_days_ago = cur_date - timedelta(days=2)
     two_days_ago = two_days_ago.strftime('%Y-%m-%d') + f" {post_time}"
 
+    # TODO: Switch to zero.
     # Check the participants of last week on Monday.
     if cur_date.weekday() == 6:
+    # if cur_date.weekday() == 0:
         jobs_types = db_query('select id from jobs_types', True)
         for job_type in jobs_types:
-            jobs = db_query(f"select * from jobs where type = {job_type[0]} order by id desc limit 2", True)
-            logger.info(f"{job_type}: {jobs}")
-            logger.info(f"{job_type}: {len(jobs)}")
+            jobs = db_query(f"select id, message_id, chat_id from jobs where type = {job_type[0]} order by id desc limit 2", True)
+            if len(jobs) == 2:
+                job_id, message_id, chat_id = jobs[0]
+                old_job_id, _, _ = jobs[1]
+
+                # TODO: remove this.
+                if old_job_id != 140:
+                    continue
+                send_notification(context, chat_id, message_id,
+                    f"select distinct(user_id) from jobs_updates where job_id = {job_id}",
+                    f"select distinct(user_id) from jobs_updates where job_id = {old_job_id}")
 
     # Check the participants of last day on other work week days.
     else:
@@ -41,36 +66,14 @@ def send_notification(context):
             if job_id != 140:
                 continue
     
-            # Get users which already have sent stickers today.
-            completed_users = set(db_query(
+            send_notification(context, chat_id, message_id,
                 f"select distinct(user_id) from jobs_updates where job_id = {job_id} and created > '{yesterday}'",
-                True,
-            ))
-            # Get all users from two previous days.
-            all_users = set(db_query(
-                f"select distinct(user_id) from jobs_updates where job_id = {job_id} and created >= '{two_days_ago}'",
-                True,
-            ))
-    
-            text = ""
-            for user_id in all_users - completed_users:
-                first_name = db_query(
-                    f"select first_name from users where id = {user_id[0]}",
-                    True,
-                )[0][0]
-                text += f'<a href="tg://user?id={user_id[0]}">{first_name}</a>, '
-    
-            if text != "":
-                text += f"день закончится через {REMINDER_DELTA} часа. Ты не забыл отметиться?"
-                logger.info(text)
-                # TODO: uncommit this.
-                #bot_message_to_chat(context, chat_id, text, reply_to_message = message_id, parse_mode = ParseMode.HTML)
-
+                f"select distinct(user_id) from jobs_updates where job_id = {job_id} and created >= '{two_days_ago}'")
 
 def reminder(job):
     # TODO: remove comments and "pass" here.
-    # job.run_daily(callback = send_notification, time = time(POST_HOUR - REMINDER_DELTA, POST_MINUTE), days = REMINDER_DAYS, name = 'reminder_ok')
-    job.run_daily(callback = send_notification, time = time(15, 45), days = [6], name = 'reminder_ok')
+    # job.run_daily(callback = reminder_callback, time = time(POST_HOUR - REMINDER_DELTA, POST_MINUTE), days = REMINDER_DAYS, name = 'reminder_ok')
+    job.run_daily(callback = reminder_callback, time = time(16, 19), days = [6], name = 'reminder_ok')
 
-    # job.run_repeating(callback = send_notification, interval = timedelta(seconds = 20), name = 'reminder_ok')
+    # job.run_repeating(callback = reminder_callback, interval = timedelta(seconds = 20), name = 'reminder_ok')
     pass
