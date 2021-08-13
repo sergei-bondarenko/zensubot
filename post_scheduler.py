3205c8c5db10b002c5c41d75de3ed4a6dacbb321
aorews <aorew@yandex.ru>
2021-08-13 14:30:46 +0300

    fix scheduler

--- post_scheduler.py
+++ post_scheduler.py
@@ -4,30 +4,27 @@ from datetime import datetime, timedelta, time
 from telegram import ParseMode
 
 from bot_functions import fill_template
-from constants import POST_WEEKDAY, POST_HOUR, POST_MINUTE, JOB_DAYS_DURATION
+from constants import POST_WEEKDAY, POST_HOUR, POST_MINUTE
 from database import db_query
 
 logger = logging.getLogger(__name__)
 
 
-def callback_minute(context):
+def post_callback(context):
     cur_date = datetime.now()
     data = db_query(
-        'select type, max(created), max(order_number)+1 from jobs group by type',
-        True,
+        """select id, jobs_type, coalesce(order_number, 0) + 1
+            from chats left join (select type, max(order_number) as order_number from jobs group by type) t on t.type = chats.jobs_type 
+            where jobs_type is not null"""
     )
-    for job_type, created, order_number in data:
-        passed_time = cur_date - created
+    for chat_id, job_type, order_number in data:
 
-        if job_type != 0 and passed_time >= timedelta(days = JOB_DAYS_DURATION):
+        if job_type != 0:
             photo_id, caption = db_query(
                 f'select photo_id, caption from post_templates where job_type = {job_type}',
                 True,
             )[0]
             caption = fill_template(caption, order_number)
-
-            chat_id = db_query(f'select id from chats where jobs_type = {job_type}', True)[0][0]
-
             
             if photo_id == "None":
                 posted_message = context.bot.send_message(chat_id, caption, parse_mode = ParseMode.HTML)
@@ -46,10 +43,13 @@ def callback_minute(context):
             
             try:
                 context.bot.unpin_chat_message(chat_id = chat_id, message_id = last_message_id)
-                # context.bot.unpin_all_chat_messages(chat_id=chat_id)
+            except:
+                logger.info(f"Unpin didn't work in chat_id {chat_id}")
+            
+            try:
                 context.bot.pin_chat_message(chat_id, posted_message.message_id)
             except:
-                logger.info(f"Pin/unpin didn't work in chat_id {chat_id}")
+                logger.info(f"Pin didn't work in chat_id {chat_id}")
     
     if context.job.name == 'post_err':
         context.job.schedule_removal()
@@ -61,7 +61,7 @@ def create_post_sc(job, completed = False):
     time_now = datetime.now()
 
     if time_now.weekday() == POST_WEEKDAY and POST_HOUR <= time_now.hour <= POST_HOUR+1 and not completed:
-        interval = timedelta(seconds = 2*60)
-        job.run_repeating(callback = callback_minute, interval = interval, last = time_now + timedelta(hours = 1), name = 'post_err')
+        interval = timedelta(minutes = 2)
+        job.run_repeating(callback = post_callback, interval = interval, last = time_now + timedelta(hours = 1), name = 'post_err')
     else:
-        job.run_daily(callback = callback_minute, time = time(POST_HOUR, POST_MINUTE), days = [POST_WEEKDAY], name = 'post_ok')
+        job.run_daily(callback = post_callback, time = time(POST_HOUR, POST_MINUTE), days = [POST_WEEKDAY], name = 'post_ok')
