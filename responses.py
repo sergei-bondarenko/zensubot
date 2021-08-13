9823e25431d05dfe3bc3233e5886b049fb70aedd
aorews <aorew@yandex.ru>
2021-08-13 13:16:57 +0300

    fix

--- responses.py
+++ responses.py
@@ -13,13 +13,13 @@ class Responses:
         #Done on startup
         data = db_query("select job_type, response_type, phrase from responses")
         for job_type, response_type, text in data:
-            cls.responses[job_type] = {response_type: text}
+            cls.responses[(job_type, response_type)] = text
 
     @classmethod
     def get(cls, job_type: int, response_type: int) -> str:
         #Gets random response if it exists or returns empty string otherwise
         try:
-            text = cls.responses[job_type][response_type]
+            text = cls.responses[(job_type, response_type)]
             phrases = [x.strip() for x in text.split("\n") if len(x) != 1]
             return random.choice(phrases)
         except KeyError:
@@ -28,4 +28,4 @@ class Responses:
     @classmethod
     def update(cls, job_type: int, response_type: int, text: str) -> None:
         #Runs after insertion of new responses to database
-        cls.responses[job_type] = {response_type: text}
+        cls.responses[(job_type, response_type)] = text
