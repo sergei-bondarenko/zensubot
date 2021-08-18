cd89968e6bba12412e7a027b9acf553d92f22549
aorews <aorew@yandex.ru>
2021-08-18 21:56:24 +0300

    test

--- zensu_bot.py
+++ zensu_bot.py
@@ -1,6 +1,6 @@
 import logging
 import os
-#testr
+#test
 
 from telegram.ext import (CallbackQueryHandler, ChatMemberHandler,
                           CommandHandler, ConversationHandler, Filters,
