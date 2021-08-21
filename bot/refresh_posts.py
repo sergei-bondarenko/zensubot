41c43ea012adc04f6c2f26e3fa4f6c876d10b10a
Sergei Bondarenko <sergei@bondarenko.xyz>
2021-08-21 16:02:24 +0100

    Fix post refresh.

--- bot/refresh_posts.py
+++ bot/refresh_posts.py
@@ -1,15 +1,20 @@
+import logging
 from database import db_query
 from datetime import time
 from constants import JOB_DAYS_DURATION, POST_HOUR, POST_MINUTE, POST_WEEKDAY
 from post_updater import PostUpdater
+
+logger = logging.getLogger(__name__)
 
 def refresh_posts(context):
     q = f"""select jobs.* , date_part('day', now()-created), 0, case when photo_id != 'None' then true else false end
                     from jobs left join post_templates on job_type = type
                     where date_part('day', now()-created) < {JOB_DAYS_DURATION} and type != 0"""
     rows = db_query(q)
+    logger.info("Refreshing posts.")
     for row in rows:
         # Example of on demand PostUpdater
+        logger.info(f"row: {row}")
         upd = PostUpdater(row)
         upd.rebuild_message(context)
 
