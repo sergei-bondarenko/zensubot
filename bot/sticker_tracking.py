f3854d6f80a46144905acccda27b6d4e9940c608
Sergei Bondarenko <sergei@bondarenko.xyz>
2022-01-29 21:06:08 +0000

    Disabled hamsa stickers.

--- bot/sticker_tracking.py
+++ bot/sticker_tracking.py
@@ -19,6 +19,17 @@ def stickers(update, context):
             )
             return
 
+        if upd.sticker_id in [56, 57, 58, 59, 60]:
+            bot_message_to_chat(
+                context,
+                update["message"]["chat"]["id"],
+                "Пятидневка больше не принимает в себя эти жуткие огромные пальцы. Слишком уж это, простите, грязное зрелище. Возможно, когда-нибудь дизайнер откопает исходники и сделает пальцы поменьше, но а пока воспользуйтесь уже <a href = 'https://t.me/addstickers/minimaal_zensu'>уменьшенными стикерами</a>.",
+                0,
+                update["message"]["chat"]["id"],
+                ParseMode.HTML
+            )
+            return
+
         # Creating new users if they do not exist or updating old users
         update_users(upd)
         # Inserting new job_update
