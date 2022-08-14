cec83804276b334bcf5bc15e3824e4a6850776ce
Sergei Bondarenko <sergei@bondarenko.xyz>
2022-08-14 08:38:07 +0100

    Fix /stat query.

--- bot/commands.py
+++ bot/commands.py
@@ -36,14 +36,50 @@ def get_stat(update: Update) -> str:
     user_id = update.effective_user["id"]
     user_name = update.effective_user["first_name"]
 
-    query = db_query(f"""select type, sum(case when max >= 4 then 1 else 0 end) as ended, count(max) as started, coalesce(sum(summ), 0)::integer
-                            from
-                                (select jobs_types."type", max(jobs_types.id) as types_id, jobs.id, max(date_part('day', jobs_updates.created - jobs.created)), sum(coalesce(stickers.power, 15 * power(2,(sticker_id-1)%5))) as summ
-                                    from jobs_types left join jobs on jobs.type = jobs_types.id left join jobs_updates on jobs.id = jobs_updates.job_id and user_id = {user_id} left join stickers on stickers.id = jobs_updates.sticker_id 
-                                    where  jobs_types.id != 0
-                                    group by jobs_types."type", jobs.id) t
-                            group by type, types_id
-                            order by types_id""")
+    query = db_query(f"""
+        SELECT
+           type,
+           SUM(CASE WHEN
+                   d0 > 0 AND
+                   d1 > 0 AND
+                   d2 > 0 AND
+                   d3 > 0 AND
+                   d4 > 0
+               THEN 1 ELSE 0 END) AS ended,
+           SUM(CASE WHEN
+                   d0 > 0 OR
+                   d1 > 0 OR
+                   d2 > 0 OR
+                   d3 > 0 OR
+                   d4 > 0 OR
+                   d5 > 0 OR
+                   d6 > 0
+               THEN 1 ELSE 0 END) AS started,
+           COALESCE(SUM(summ), 0)::INTEGER
+        FROM
+            (
+                SELECT
+                    jobs_types."type",
+                    MAX(jobs_types.id) AS types_id,
+                    jobs.id,
+                    SUM(CASE WHEN DATE_PART('day', jobs_updates.created - jobs.created) = 0 THEN power ELSE 0 END) d0,
+                    SUM(CASE WHEN DATE_PART('day', jobs_updates.created - jobs.created) = 1 THEN power ELSE 0 END) d1,
+                    SUM(CASE WHEN DATE_PART('day', jobs_updates.created - jobs.created) = 2 THEN power ELSE 0 END) d2,
+                    SUM(CASE WHEN DATE_PART('day', jobs_updates.created - jobs.created) = 3 THEN power ELSE 0 END) d3,
+                    SUM(CASE WHEN DATE_PART('day', jobs_updates.created - jobs.created) = 4 THEN power ELSE 0 END) d4,
+                    SUM(CASE WHEN DATE_PART('day', jobs_updates.created - jobs.created) = 5 THEN power ELSE 0 END) d5,
+                    SUM(CASE WHEN DATE_PART('day', jobs_updates.created - jobs.created) = 6 THEN power ELSE 0 END) d6,
+                    SUM(COALESCE(stickers.power, 15 * power(2,(sticker_id-1)%5))) AS summ
+                FROM jobs_types
+                    LEFT JOIN jobs ON jobs.type = jobs_types.id
+                    LEFT JOIN jobs_updates on jobs.id = jobs_updates.job_id AND user_id = {user_id}
+                    LEFT JOIN stickers ON stickers.id = jobs_updates.sticker_id
+                WHERE jobs_types.id != 0
+                GROUP BY jobs_types."type", jobs.id
+            ) t
+        GROUP BY type, types_id
+        ORDER BY types_id
+    """)
     
     text = f'<b>Статистика пятидневок {user_name}</b><br>'
     text += f"<pre> ‎ ‎ ‎ ‎ ‎  ‎Тип ‎ ‎ ‎ ‎ ‎ ‎Закончено ‎‏‏‎ ‎Время<br>"
