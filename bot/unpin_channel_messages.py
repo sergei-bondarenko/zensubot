9169fa3e72cc78cefa3f1ceba6de3965c1e9b26d
Sergei Bondarenko <sergei@bondarenko.xyz>
2023-03-27 21:33:21 +0100

    Exclude yoga channel from unpin.

--- bot/unpin_channel_messages.py
+++ bot/unpin_channel_messages.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 def unpin_channel_messages(update: Update, context: CallbackContext) -> None:
     """Unpins messages from channels in chats"""
-    if update.effective_message.forward_from_chat and update.effective_message.forward_from_chat.type == 'channel' and update.effective_message.forward_from_chat.id == -1001216918794:
+    if update.effective_message.forward_from_chat and update.effective_message.forward_from_chat.type == 'channel' and update.effective_message.forward_from_chat.id != -1001216918794:
         logger.info(f"Unpinning message: {update}")
         try:
             update.effective_message.unpin()
